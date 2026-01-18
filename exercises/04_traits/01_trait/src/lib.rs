14b52a5486654fe32e8ca3e25b65523161e5a9d9
Cameron <milnec04@gmail.com>
2026-01-18 16:35:54 +0000

    Traits 00, 01, 02, 03

--- exercises/04_traits/01_trait/src/lib.rs
+++ exercises/04_traits/01_trait/src/lib.rs
@@ -2,10 +2,28 @@
 // even, otherwise `false`.
 //
 // Then implement the trait for `u32` and `i32`.
+mod dick {
+    pub trait IsEven {
+        fn is_even(self) -> bool;
+    }
+
+    impl IsEven for u32 {
+        fn is_even(self) -> bool {
+            self % 2 == 0
+        }
+    }
+    impl IsEven for i32 {
+        fn is_even(self) -> bool {
+            self % 2 == 0
+        }
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use dick::IsEven;
 
     #[test]
     fn test_u32_is_even() {
