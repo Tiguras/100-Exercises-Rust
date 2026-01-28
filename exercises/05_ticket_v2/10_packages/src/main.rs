2873af9f40b2944198327b7a8bb14d6b4769b266
Cameron <milnec04@gmail.com>
2026-01-28 10:03:40 +0000

    thiserror

--- exercises/05_ticket_v2/10_packages/src/main.rs
+++ exercises/05_ticket_v2/10_packages/src/main.rs
@@ -6,6 +6,16 @@
 use packages::hello_world;
 
 // This is the entrypoint of the binary.
+
+// A crate is the smallest unit of compilation in Rust. It's a tree of modules that produces
+// either a library or an executable. There are two types, Binary crates and Library crates
+
+// A packages is a bundle of one or more crates. It contains:
+// - A Cargo.toml
+// - At most one library crate
+// - Any number of binary crates
+// - At least one crate 
+
 fn main() {
     hello_world();
 }
